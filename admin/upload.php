de6d51534ba7b6bde9954d281d8bbfcb84c7728c
Sayem12346 <124008357+Sayem12346@users.noreply.github.com>
2026-06-10 08:21:52 +0600

    Delete admin/upload.php

--- admin/upload.php
+++
@@ -1,1 +0,0 @@
-<form method='post' enctype='multipart/form-data'><input type='file' name='playlist' accept='.m3u,.m3u8'><button>Upload</button></form><?php echo 'Implement parser for authorized content.';?>
